b95f335ad9b1a560df2945388af4f6f24a5befb5
Joseph L. Casale <9957114+jcasale@users.noreply.github.com>
2023-11-17 13:46:01 -0700

    Updates debug sample.

--- src/PSSerilog/Debug.ps1
+++ src/PSSerilog/Debug.ps1
@@ -2,4 +2,17 @@ Set-StrictMode -Version Latest
 
 Import-Module .\PSSerilog.psd1
 
-$expressionTemplate = "[{@t:yyyy-MM-dd HH:mm:ss.fff}] [{@l}] {@m:l}{#if Rest() <> {}}`n`t{Rest()}{#end}`n{@x}"
+$template = '[{Timestamp:yyyy-MM-dd HH:mm:ss.fff}] [{Level}] [{MyValue}] {Message:l}{NewLine}{Exception}'
+
+$configuration = New-SerilogLoggerConfiguration -MinimumLevel Verbose -GlobalContext |
+  Add-SerilogSinkConsole -OutputTemplate $template
+
+$logger = New-SerilogLogger -Configuration $configuration
+
+$context = New-SerilogGlobalContext -Name MyValue -Value 42
+$logger.Information('Message 1')
+$context.Dispose()
+
+write-host ""
+
+$logger.Information('Message 2')
